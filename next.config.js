404ae078851fd99478cf4f38def8a0c3f9cd14bc
Estefania <estefania.osses.v@gmail.com>
2024-10-01 02:01:11 -0300

    Build configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   cacheHandler: require.resolve('./cache-handler.js'),
   cacheMaxMemorySize: 0, // disable default in-memory caching,
+  output:"standalone",
   images: {
     remotePatterns: [
       {
