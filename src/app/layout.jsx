7de2a2d149cfeb38f1e11a6b5ab38f75017ae089
Estefania <estefania.osses.v@gmail.com>
2024-10-10 23:57:34 -0300

    Activate Hotjar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -38,17 +38,17 @@ export default async function RootLayout({ children }) {
 
         <Script src="https://kit.fontawesome.com/7a6fedca6c.js" ></Script>
         <Script id="fontawesome" src="https://kit.fontawesome.com/a790242b27.js" ></Script>
-        {/* <Script id="hotjar" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        <Script id="hotjar" strategy="afterInteractive" dangerouslySetInnerHTML={{
           __html:
             `(function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:3921307,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`
-        }}></Script> */}
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:5167226,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+        }}></Script>
       </head>
       <body className={`${roboto_init.variable}`}>
         <SectionProvider>
